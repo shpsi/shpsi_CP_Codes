6f386a187c913795150804ce946d60963c992dee
Shiv Pratap Singh <shiv.si@media.net>
2019-10-14 17:46:29 +0530

    downloaded cf problems

--- 1201/a/a.cpp
+++ 1201/a/a.cpp
@@ -1,43 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, m;
-string s[1005];
-ll a[1005];
-ll ans;
-ll b[1005][5];
-ll ma;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin>>n>>m;
-    for (ll i=1; i<=n; i++)
-    {
-        cin>>s[i];
+int main(){
 
-    }
-    for (ll i=0; i<m; i++)
-    {
-        cin>>a[i];
-
-    }
-    for (ll i=0; i<m; i++)
-    {
-        ma=0;
-        for (ll j=1; j<=n; j++)
-        {
-            b[i][s[j][i]-'A']++;
-
-        }
-        for (ll j=0; j<5; j++)
-        {
-            ma=max(ma, b[i][j]);
-
-        }
-        ans+=ma*a[i];
-
-    }
-    cout<<ans;
-
-}
-
+    return 0;
+}    
